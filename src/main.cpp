3a7c316fe4fcf5d1095c9865b8d8155c932252e8
Olivia Stanley <Oliviatinkerbell7@gmail.com>
2025-11-11 15:22:36 +0100

    Fix GetModuleHandle wide-char issue for MinGW

--- src/main.cpp
+++ src/main.cpp
@@ -1,35 +1,32 @@
 #include <windows.h>
 #include <fstream>
-#include <stdint.h>
+#include "cheats.h"
 
 DWORD WINAPI CheatThread(LPVOID lpParam) {
     std::ofstream log("C:\\Users\\Public\\hook_log.txt", std::ios::app);
     log << "DLL successfully hooked!\n";
     log.close();
 
-    HMODULE hMod = GetModuleHandle(L"GameLogic.dll");
-    if (!hMod) {
-        while (!(hMod = GetModuleHandle(L"GameLogic.dll"))) {
-            Sleep(1000);
-        }
+    // Use ANSI version since MinGW expects LPCSTR, not LPCWSTR
+    HMODULE hGameLogic = GetModuleHandleA("GameLogic.dll");
+    while (!hGameLogic) {
+        Sleep(100);
+        hGameLogic = GetModuleHandleA("GameLogic.dll");
     }
 
-    volatile int* health = (int*)((uintptr_t)hMod + 0x4E2230);
-    volatile int* xp     = (int*)((uintptr_t)hMod + 0x4F7B74);
-    volatile int* tech   = (int*)((uintptr_t)hMod + 0x4F7B78);
+    volatile int* health = (int*)((uintptr_t)hGameLogic + HEALTH_ADDR);
+    volatile int* xp     = (int*)((uintptr_t)hGameLogic + XP_ADDR);
+    volatile int* tech   = (int*)((uintptr_t)hGameLogic + TECH_ADDR);
 
     MessageBoxA(NULL, "Progamingidea hook is running!", "DLL Loaded", MB_OK);
 
     while (true) {
-        try {
-            if (health) *health = 9999;
-            if (xp) *xp = 9999;
-            if (tech) *tech = 9999;
-        } catch (...) {
-            // ignore any memory write errors
-        }
+        if (health) *health = 9999;
+        if (xp) *xp = 9999;
+        if (tech) *tech = 9999;
         Sleep(100);
     }
+
     return 0;
 }
 
