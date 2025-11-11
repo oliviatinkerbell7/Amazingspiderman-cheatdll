0ef359340ef983ad9bda016db418fae47a73f830
Olivia Stanley <Oliviatinkerbell7@gmail.com>
2025-11-11 15:27:00 +0100

    Add missing cheats.h header

--- include/cheats.h
+++ include/cheats.h
@@ -1,6 +1,9 @@
 #pragma once
 
-// Example offsets (kept also in src/main.cpp)
-#define GAMELOGIC_HEALTH_OFFSET 0x004E2230
-#define GAMELOGIC_XP_OFFSET     0x004F7B74
-#define GAMELOGIC_TECH_OFFSET   0x004F7B78
+#include <windows.h>
+
+void EnableInfiniteHealth();
+void EnableInfiniteAmmo();
+void EnableSuperJump();
+void DisableAllCheats();
+
